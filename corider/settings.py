a4632e94b4e5a4f1e98ad82607e2d23bd5984c14
horpeazy <iyamuhope.nosa647@gmail.com>
2023-05-28 06:00:54 +0100

    feat: dockerfile

--- corider/settings.py
+++ corider/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.urandom(32)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://corider.hopeiyamu.tech']
